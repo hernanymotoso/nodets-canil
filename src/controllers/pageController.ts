e39180a8fcb46954a9b60b0d9c474d685dc0abb2
Hernany Motoso <contatohernany@gmail.com>
2022-04-08 20:26:31 -0300

    feat: showing animals by type

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,42 +1,51 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Request, Response } from 'express';
 import { createMenuObject } from '../helpers/createMenuObject';
+import { Pet } from '../models/Pet';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const home = (req: Request, res: Response) => {
+  const list = Pet.getAll();
   res.render('pages/page', {
     menu: createMenuObject('all'),
     banner: {
       title: 'All animals',
       background: 'allanimals.jpg',
     },
+    list,
   });
 };
 const dogs = (req: Request, res: Response) => {
+  const list = Pet.getFromType('dog');
   res.render('pages/page', {
     menu: createMenuObject('dog'),
     banner: {
       title: 'Dogs',
       background: 'banner_dog.jpg',
     },
+    list,
   });
 };
 const cats = (req: Request, res: Response) => {
+  const list = Pet.getFromType('cat');
   res.render('pages/page', {
     menu: createMenuObject('cat'),
     banner: {
       title: 'Cats',
       background: 'banner_cat.jpg',
     },
+    list,
   });
 };
 const fishes = (req: Request, res: Response) => {
+  const list = Pet.getFromType('fish');
   res.render('pages/page', {
     menu: createMenuObject('fish'),
     banner: {
       title: 'Fishes',
       background: 'banner_fish.jpg',
     },
+    list,
   });
 };
 
