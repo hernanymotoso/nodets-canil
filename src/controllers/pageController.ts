74ea72220a87c4ad99a195296e6996f6fe572b91
Hernany Motoso <contatohernany@gmail.com>
2022-04-08 19:10:52 -0300

    feat: passing the variables to the page and header

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,19 +1,43 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Request, Response } from 'express';
+import { createMenuObject } from '../helpers/createMenuObject';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const home = (req: Request, res: Response) => {
-  res.send('home page');
-  console.log('page/paged');
+  res.render('pages/page', {
+    menu: createMenuObject('all'),
+    banner: {
+      title: 'All animals',
+      background: 'allanimals.jpg',
+    },
+  });
 };
 const dogs = (req: Request, res: Response) => {
-  console.log('page/paged');
+  res.render('pages/page', {
+    menu: createMenuObject('dog'),
+    banner: {
+      title: 'Dogs',
+      background: 'banner_dog.jpg',
+    },
+  });
 };
 const cats = (req: Request, res: Response) => {
-  console.log('page/paged');
+  res.render('pages/page', {
+    menu: createMenuObject('cat'),
+    banner: {
+      title: 'Cats',
+      background: 'banner_cat.jpg',
+    },
+  });
 };
 const fishes = (req: Request, res: Response) => {
-  console.log('page/paged');
+  res.render('pages/page', {
+    menu: createMenuObject('fish'),
+    banner: {
+      title: 'Fishes',
+      background: 'banner_fish.jpg',
+    },
+  });
 };
 
 // eslint-disable-next-line import/prefer-default-export
